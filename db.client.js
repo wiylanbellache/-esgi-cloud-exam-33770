d2b9aab9cf3d252274d33f2ea73cb234e61bee04
wiylanbellache <bellachewiylan@gmail.com>
2023-12-15 10:34:14 +0100

    db

--- db.client.js
+++ db.client.js
@@ -1,23 +1,29 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  'blender', // Database name
+  'blender_user', // User
+  'WTYOSlOnEOKkkrGx1YDx9yqaUHjt2ozv', // Password
   {
+    host: 'dpg-ck9d2370vg2c73djgn60-a', // Host
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
-  },
+    define: {
+      createdAt: 'added',
+      updatedAt: 'updated',
+    },
+  }
 );
 
 // authentication and synchronization
 sequelize.authenticate()
-  .then(() => {
-    sequelize.sync().catch(() => console.log("Cannot sync the database"));
-  })
-  .catch(() => console.log("Cannot connect to database, please check environment credentials"));
+  .then(() => sequelize.sync())
+  .catch(() => console.log("Cannot connect to the database, please check environment credentials"));
 
 module.exports = sequelize;
