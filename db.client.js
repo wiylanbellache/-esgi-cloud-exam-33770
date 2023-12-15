17d5d5b64ead7a06abcdbe12b46bf215cbe69c97
wiylanbellache <bellachewiylan@gmail.com>
2023-12-15 10:44:03 +0100

    db2

--- db.client.js
+++ db.client.js
@@ -1,29 +1,23 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'blender', // Database name
-  'blender_user', // User
-  'WTYOSlOnEOKkkrGx1YDx9yqaUHjt2ozv', // Password
+  'postgres://blender_user:WTYOSlOnEOKkkrGx1YDx9yqaUHjt2ozv@dpg-ck9d2370vg2c73djgn60-a.frankfurt-postgres.render.com/blender', // TODO
   {
-    host: 'dpg-ck9d2370vg2c73djgn60-a', // Host
-    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
-    define: {
-      createdAt: 'added',
-      updatedAt: 'updated',
-    },
-  }
+  },
 );
 
 // authentication and synchronization
 sequelize.authenticate()
-  .then(() => sequelize.sync())
-  .catch(() => console.log("Cannot connect to the database, please check environment credentials"));
+  .then(() => {
+    sequelize.sync().catch(() => console.log("Cannot sync the database"));
+  })
+  .catch(() => console.log("Cannot connect to database, please check environment credentials"));
 
 module.exports = sequelize;
