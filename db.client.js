98940411f2b24ecfdef0c1037bb00684d5aa77f8
wiylanbellache <bellachewiylan@gmail.com>
2023-12-15 10:53:55 +0100

    dbWithoutId

--- db.client.js
+++ db.client.js
@@ -1,23 +1,37 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 // database
 const sequelize = new Sequelize(
-  'postgres://blender_user:WTYOSlOnEOKkkrGx1YDx9yqaUHjt2ozv@dpg-ck9d2370vg2c73djgn60-a.frankfurt-postgres.render.com/blender', // TODO
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
+    define: {
+      createdAt: 'added',
+      updatedAt: 'updated',
+    },
   },
 );
 
 // authentication and synchronization
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
     sequelize.sync().catch(() => console.log("Cannot sync the database"));
   })
-  .catch(() => console.log("Cannot connect to database, please check environment credentials"));
+  .catch(() =>
+    console.log(
+      "Cannot connect to database, please check environment credentials"
+    )
+  );
 
 module.exports = sequelize;
